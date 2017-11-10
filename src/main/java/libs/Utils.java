0e68fbf92f1378d86604bb0594388581b93b8191
RainbowG26 <lem.feather@yandex.ru>
2017-11-10 13:27:58 +0200

    Create Libs - Utils Update parentTest - ParentTests Date: 08/11/17

---
+++ src/main/java/libs/Utils.java
@@ -0,0 +1,46 @@
+package libs;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Utils {
+
+    private Logger log;
+
+    public Utils(){ //Создали конструктор для работы с логом
+        log = Logger.getLogger(getClass());
+    }
+
+    /**
+     * Taking screenshot into .//target// + pathToScreenShot
+     * @param pathToScreenShot
+     * @param driver
+     */
+    public void screenShot(String pathToScreenShot, WebDriver driver){ //Метод который делает скрин
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); //Снимает скрин
+        try {
+            FileUtils.copyFile(scrFile, new File(pathToScreenShot));
+            log.info("ScreenShot: " + pathToScreenShot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Hard wait
+     * @param second
+     */
+    public static void waitABit(int second){ //Метод останавливает все процессы в джава
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
